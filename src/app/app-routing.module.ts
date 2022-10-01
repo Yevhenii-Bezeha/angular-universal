31ee06a9c553eabf2627914f1c018ecf36c4d89d
YevheniiBez <yevhenii.bezeha@intellias.com>
2022-10-01 19:14:39 +0300

    Add app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {CourseComponent} from "./course/course.component";
-import {CourseResolver} from "./services/course.resolver";
-import {AboutComponent} from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'courses/:id',
-    component: CourseComponent,
-    resolve: {
-      course: CourseResolver
-    }
-  },
-  {
-    path: "**",
-    redirectTo: '/'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    relativeLinkResolution: 'legacy'
+    initialNavigation: 'enabledBlocking'
 })],
   exports: [RouterModule]
 })
