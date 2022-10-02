4073905f550ad56955281b68d5642a688a2fc5b1
YevheniiBez <yevhenii.bezeha@intellias.com>
2022-10-02 12:08:10 +0300

    Add app shell render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppShellRenderDirective } from './app-shell-render-directive';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppShellNoRenderDirective } from './app-shell-norender.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppShellRenderDirective,
+    AppShellNoRenderDirective
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
